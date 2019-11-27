499b1ed73e25b8a465749b4501191c60dd3a2303
mikeNG <mkbestas@lineageos.org>
2019-11-27 18:04:25 +0530

    Switch to device/qcom/sepolicy-legacy-um policy
    *Also Ignore Neverallows
    
    Signed-off-by: Melvin <kartik3163@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -204,8 +204,9 @@ endif
 VENDOR_SECURITY_PATCH := 2019-05-01
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
